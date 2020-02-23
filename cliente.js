24470598e498a3e5ca51b8269d6adf455c2bbca9
Juan Ramos <jramos32@ucol.mx>
2020-02-23 01:36:30 -0600

    getPerfil

--- cliente.js
+++ cliente.js
@@ -1,5 +1,11 @@
-class Cliente{
-    constructor(){
-        
+import Direccion from "./direccion.js"
+export default class Cliente{
+    constructor(nombre,direccion,telefono){
+        this.nombre=nombre;
+        this.direccion=direccion;
+        this.telefono=telefono;
+    }
+    getPerfil(){
+        return(`${this.nombre}, ${this.direccion.getFormatoExtendido()}, ${this.telefono}`)
     }
 }

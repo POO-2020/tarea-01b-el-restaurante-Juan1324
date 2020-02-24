d089d8af0d688595b9c9bc427b90db2db2dd6bfd
Juan Ramos <jramos32@ucol.mx>
2020-02-24 11:23:28 -0600

    probarTiempo

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 import Direccion from "./direccion.js"
 import Cliente from "./cliente.js"
+import Tiempo from "./tiempo.js"
 
 class Main{
     constructor(){
         
         this.direccion1=new Direccion("Bahía de Manzanillo","#234","#234-A","Solidaridad","29878","Villa de Álvarez","Villa de Álvarez")
         this.cliente1= new Cliente("Omar Pérez",this.direccion1,"312-109-1987")
+        this.hora1= new Tiempo(3,40,"pm")
         
+    }
+    
+    probarTiempo(){
+    console.log("////////////////////Tiempo///////////////////////////////////")
+    console.log(this.hora1.getFormato12())
+    console.log(this.hora1.getFormato24())
 
     }
     probarCliente(){
+        console.log("////////////////////Cliente///////////////////////////////////")
         console.log(`${this.cliente1.getPerfil()}`);
 
     }
 }
 let app=new Main();
 app.probarCliente();
+app.probarTiempo();

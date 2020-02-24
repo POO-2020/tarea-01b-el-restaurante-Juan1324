d2c992dff218a38c7bb46e730582005477f52887
Juan Ramos <jramos32@ucol.mx>
2020-02-24 11:38:12 -0600

    probarProducto

--- producto.js
+++ producto.js
@@ -1,5 +1,12 @@
-class Producto{
-    constructor(){
-        
+import Precio from "./precio.js"
+export default class Producto{
+
+    constructor(nombre,precio){
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    getDescripcion(){
+        return `${this.nombre} ${this.precio.getPrecio()}`
     }
 }

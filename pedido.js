692423296f8e35ae24a946eaa934475570001ada
Juan Ramos <jramos32@ucol.mx>
2020-02-24 13:33:47 -0600

    constructor

--- pedido.js
+++ pedido.js
@@ -1,5 +1,12 @@
+import Precio from "./precio.js";
+import Cliente from "./cliente.js";
+import Producto from "./producto.js";
+import ElementoPedido from "./elementoPedido.js";
+
 export default class Pedido{
-    constructor(){
-        
+    constructor(fecha,hora,cliente){
+        this.fecha = fecha;
+        this.hora = hora;
+        this.cliente = cliente;
+        this.NuelementosPedidos = []
     }
-}

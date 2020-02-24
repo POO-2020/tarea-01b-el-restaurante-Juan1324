1a155bb9bf55c3bc411287a545b6b6ff17955725
Juan Ramos <jramos32@ucol.mx>
2020-02-24 11:47:52 -0600

    probarProducto

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Direccion from "./direccion.js"
 import Cliente from "./cliente.js"
 import Tiempo from "./tiempo.js"
 import Precio from "./precio.js"
+import Producto from "./producto.js"
 
 class Main{
     constructor(){
@@ -9,7 +10,8 @@ class Main{
         this.direccion1=new Direccion("Bahía de Manzanillo","#234","#234-A","Solidaridad","29878","Villa de Álvarez","Villa de Álvarez")
         this.cliente1= new Cliente("Omar Pérez",this.direccion1,"312-109-1987")
         this.hora1= new Tiempo(3,40,"pm")
-        this.valor=new Precio(10.50) 
+        this.valor=new Precio(10.51)
+        this.producto=new Producto("Galletas",this.valor) 
         
     }
     
@@ -33,6 +35,10 @@ class Main{
         console.log("////////////////////Precio///////////////////////////////////") 
         console.log(this.valor.getPrecio())
     }
+    probarProducto(){
+        console.log("////////////////////Producto///////////////////////////////////") 
+        console.log(this.producto.getDescripcion())
+    }
 
 
 }
@@ -41,3 +47,4 @@ app.probarCliente();
 app.probarTiempo();
 app.probarDireccion();
 app.probarPrecio();
+app.probarProducto();

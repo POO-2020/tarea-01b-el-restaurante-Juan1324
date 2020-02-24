2ab1352d1b9cbd409f5ef6a0c1a86720b3f24da4
Juan Ramos <jramos32@ucol.mx>
2020-02-24 11:16:52 -0600

    getFormato12

--- tiempo.js
+++ tiempo.js
@@ -1,5 +1,12 @@
-class Tiempo{
-    constructor(hora,minuto,periodo){
-
+export default class Tiempo{
+    constructor(hora,minutos,periodo){
+        this.hora=hora;
+        this.minutos=minutos;
+        this.periodo=periodo;
     }
-}
+        
+
+        getFormato12(){
+            return `${this.hora}:${this.minutos} ${this.periodo}`
+        }
+    }
